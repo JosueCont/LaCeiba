f94322218d86776b3f2c8f98659be639adf5e4ef
eduardocouoh <eduardo.couoh@hiumanlab.com>
2022-08-10 17:29:10 -0500

    * Correcciones en modales, textos, flujos en general del flujo de seguridad

--- navigation/NavigationContainerMain.js
+++ navigation/NavigationContainerMain.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSelector} from "react-redux";
 import {NavigationContainer} from "@react-navigation/native";
 import StackNavigatorSecurity from "./StackNavigatorSecurity";
 import StackNavigatorMain from "./StackNavigatorMain";
+import {Colors} from "../Colors";
+import {Spinner, View} from "native-base";
 
 const NavigatorContainerMain = () => {
     const status = useSelector(state => {
         return state.appDuck.logged;
     });
+    const [loading, setLoading] = useState(null)
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 100)
+    }, [status]);
+
+    console.log(status)
 
 
     return (
         <NavigationContainer>
             {
+                loading ?
+                    <View flex={1} backgroundColor={Colors.green} alignItems={'center'} justifyContent={'center'}>
+                        <Spinner size={'sm'} color={'white'}></Spinner>
+                    </View> :
                     status === true ? <StackNavigatorMain/> : <StackNavigatorSecurity/>
             }
         </NavigationContainer>
