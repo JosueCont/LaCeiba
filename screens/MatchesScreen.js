35c8c1352d6aea7621ba6f6c25e1d9ba4daf9d57
eduardocouoh <eduardo.couoh@hiumanlab.com>
2022-08-22 10:00:44 -0500

    * Pantalla partido fijos, corrección.

--- screens/MatchesScreen.js
+++ screens/MatchesScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
-import {Text, View} from "native-base";
+import {Button, Icon, Text, View} from "native-base";
 import {Colors} from "../Colors";
 import LayoutV4 from "./Layouts/LayoutV4";
 import MatchItem from "./MatchItem";
+import {MaterialIcons} from "@expo/vector-icons";
 
 const MatchesScreen = ({navigation}) => {
 
@@ -12,6 +13,19 @@ const MatchesScreen = ({navigation}) => {
             <View flex={1} mx={8}>
 
                 <Text textAlign={'center'} mt={10} mb={5} color={Colors.green} fontFamily={'titleComfortaaBold'} fontSize={'2xl'}>Próximos partidos</Text>
+
+                <View flexDirection={'row'} mb={4}>
+                    <View flex={1} p={2}>
+                        <Button leftIcon={<Icon as={MaterialIcons} name="date-range" size="sm"/>}>
+                            Buscar partido
+                        </Button>
+                    </View>
+                    <View flex={1} p={2}>
+                        <Button leftIcon={<Icon as={MaterialIcons} name="add" size="sm"/>}>
+                            Nuevo partido
+                        </Button>
+                    </View>
+                </View>
 
                 <MatchItem mb={4} yellow={true}/>
                 <MatchItem mb={4}/>
