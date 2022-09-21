5e38044ccd1e51299cb821bea913fbfa5c1f3eb1
eduardocouoh <eduardo.couoh@hiumanlab.com>
2022-09-21 16:36:07 -0500

    * Invitados flujo

--- app.config.js
+++ app.config.js
@@ -30,13 +30,13 @@ const commonConfig = {
 };
 
 const ios = {
-    buildNumber: "8",
+    buildNumber: "9",
     supportsTablet: false,
     bundleIdentifier: "com.hiumanlab.clublahacienda"
 };
 
 const android = {
-    versionCode: 7,
+    versionCode: 8,
     adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundImage: "./assets/adaptive-icon-bg.png"
