812c3ce1d65b27e285c89b18b99528b02b758b57
eduardocouoh <eduardo.couoh@hiumanlab.com>
2022-10-05 17:07:52 -0500

    * Correcciones

--- app.config.js
+++ app.config.js
@@ -30,13 +30,13 @@ const commonConfig = {
 };
 
 const ios = {
-    buildNumber: "15",
+    buildNumber: "16",
     supportsTablet: false,
     bundleIdentifier: "com.hiumanlab.clublahacienda"
 };
 
 const android = {
-    versionCode: 14,
+    versionCode: 15,
     adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundImage: "./assets/adaptive-icon-bg.png"
