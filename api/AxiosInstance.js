fb0bacaa8b5fd0df0bf2e84c5ab570f088005ce9
eduardocouoh <eduardo.couoh@hiumanlab.com>
2022-10-24 11:27:39 -0500

    * Buscar personas

--- api/AxiosInstance.js
+++ api/AxiosInstance.js
@@ -20,29 +20,29 @@ axiosInstance.interceptors.request.use(async (request) => {
             let isExpired = false;
             let decodedToken = jwtDecode(token.access_token);
             console.log(decodedToken)
-            // let dateNow = new Date();
-            // if (decodedToken.exp < dateNow.getTime()) {
-            //     console.log('expired:', true)
-            //     isExpired = true
-            //     try {
-            //         let baseURL = Constants.manifest.extra.production ? Constants.manifest.extra.URL : Constants.manifest.extra.URL_DEV;
-            //         const response = await axios.post(`${baseURL}/v1/auth/refresh`, {
-            //             refresh_token: token.refresh_token
-            //         })
-            //
-            //
-            //         request.headers.Authorization = `Bearer ${response.data.access_token}`;
-            //
-            //
-            //     } catch (e) {
-            //         console.log(e.response.data, 43)
-            //     }
-            //
-            // } else {
-            //     console.log('expired:', false)
-            request.headers.Authorization = `Bearer ${token.access_token}`;
+            let dateNow = new Date();
+            if (decodedToken.ist > Date.now()) {
+                console.log('expired:', true)
+                isExpired = true
+                try {
+                    let baseURL = Constants.manifest.extra.production ? Constants.manifest.extra.URL : Constants.manifest.extra.URL_DEV;
+                    const response = await axios.post(`${baseURL}/v1/auth/refresh`, {
+                        refresh_token: token.refresh_token
+                    })
 
-            //}
+
+                    request.headers.Authorization = `Bearer ${response.data.access_token}`;
+
+
+                } catch (e) {
+                    console.log(e, 43)
+                }
+
+            } else {
+                console.log('expired:', false)
+                request.headers.Authorization = `Bearer ${token.access_token}`;
+
+            }
         }
 
     } catch (e) {
