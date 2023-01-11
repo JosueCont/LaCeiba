8256cfe2a8503d7673b32face04c1a1eb0ccb716
Isaac Euan <isaac.euan@hiumanlab.com>
2023-01-11 09:44:34 -0600

    modificacion items vista notificaciones

--- screens/NotificationItem.js
+++ screens/NotificationItem.js
@@ -25,14 +25,14 @@ const NotificationItem = ({navigation, mb = 5, notification}) => {
         <TouchableOpacity onPress={()=>{navigation.navigate('NotificationDetail', {notification: notification.id});}}>
             <View flexDirection={'row'} justifyContent={'center'} padding={2} bgColor={'#fff'} borderRadius={50} mb={5}>
           
-            <View width={'85%'} justifyContent={'center'} alignItems={'center'}>
+            <View justifyContent={'center'} alignItems={'center'}>
                 <Text  mb={1} color={Colors.green} fontSize={'md'}>{notification?.template?.title}</Text>
                 <Text  color={Colors.green} fontSize={'xs'}>{typeCategory(notification?.template?.category)}</Text>
                 {/* <Text color={Colors.green} fontSize={'xs'} width={'90%'}>{detail}</Text> */}
             </View>
-            <View width={'10%'}  justifyContent={'center'} alignItems={'center'}>
+            <View position={'absolute'} right={'5'} top={'5'} justifyContent={'center'} alignItems={'center'}>
                     <Image source={IconNotificationRead}></Image>
-                </View>
+            </View>
         </View>
         </TouchableOpacity>
         
