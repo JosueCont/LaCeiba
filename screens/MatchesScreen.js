f177ec3e20f39a5d9f7e655b9490c62a5c6b9464
Isaac Euan <isaac.euan@hiumanlab.com>
2023-02-16 17:52:56 -0600

    pantalla de partidos agregada

--- screens/MatchesScreen.js
+++ screens/MatchesScreen.js
@@ -1,40 +1,53 @@
 import React from "react";
-import {Button, Icon, Text, View} from "native-base";
+import {Button, Icon, Text, View, Image} from "native-base";
+import iconGroupSmall from "../assets/iconGroupSmall.png";
 import {Colors} from "../Colors";
 import LayoutV4 from "./Layouts/LayoutV4";
 import MatchItem from "./MatchItem";
 import {MaterialIcons} from "@expo/vector-icons";
+import { color } from "react-native-reanimated";
 
 const MatchesScreen = ({navigation}) => {
 
 
     return (
         <LayoutV4>
-            <View flex={1} mx={8}>
-
-                <Text textAlign={'center'} mt={10} mb={5} color={Colors.green} fontFamily={'titleComfortaaBold'} fontSize={'2xl'}>Próximos partidos</Text>
-
-                <View flexDirection={'row'} mb={4}>
+            <View flex={1} mx={8} mb={5}>
+            <View flexDirection={'row'} mt={5}>
                     <View flex={1} p={2}>
-                        <Button leftIcon={<Icon as={MaterialIcons} name="date-range" size="sm"/>}>
+                        <Button p={2}  leftIcon={<Icon as={MaterialIcons} color={Colors.green} name="date-range" size="sm"/>} variant="outline" borderColor={Colors.green} _text={{color: Colors.green}}>
                             Buscar partido
                         </Button>
                     </View>
                     <View flex={1} p={2}>
-                        <Button leftIcon={<Icon as={MaterialIcons} name="add" size="sm"/>}>
-                            Nuevo partido
+                        <Button  p={2} textAlign={'center'} justifyContent={'center'} alignItems={'center'}>
+                        Tabla de registro
                         </Button>
                     </View>
                 </View>
 
+                <Text textAlign={'center'} mt={2} color={Colors.green} fontFamily={'titleComfortaaBold'} fontSize={'2xl'}>PRÓXIMOS JUEGOS</Text>
+                <View mt={1} alignItems={'center'} flexDirection={'row'} justifyContent='center' mb={4}>
+                <Image mr={1} source={iconGroupSmall} style={{width: 15, height: 15}}></Image>
+                <Text textAlign={'center'} color={Colors.green} fontFamily={'titleComfortaaBold'} fontSize={'xs'}>GRUPO FIJO</Text>
+                </View>
+        
                 <MatchItem mb={4} yellow={true}/>
                 <MatchItem mb={4}/>
-                <MatchItem mb={4}/>
-                <Text textAlign={'center'} mb={4} color={Colors.green} fontSize={'sm'}>Partidos finalizados</Text>
+                <View mt={4} flexDirection={'row'} justifyContent='center' mb={4}>
+                <View mt={3} mr={2} width={'15%'} background={Colors.green} height={'2px'}/>
+                <Text textAlign={'center'} mb={4} color={Colors.green} fontSize={'md'}>PARTIDOS FINALIZADOS</Text>
+                <View mt={3} ml={2} width={'15%'} background={Colors.green} height={'2px'}/>
+
+                </View>
 
                 <MatchItem mb={4}/>
                 <MatchItem mb={4}/>
                 <MatchItem mb={4}/>
+                <MatchItem mb={4}/>
+                <MatchItem mb={4}/>
+                <MatchItem mb={4}/>
+            
             </View>
 
         </LayoutV4>
