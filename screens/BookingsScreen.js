e289c6b7355f9f32b317a1f2bc3080ff5384b9f7
Isaac Euan <isaac.euan@hiumanlab.com>
2023-03-14 17:32:28 -0600

    validación de reservaciones si no trae un BokingId asociado

--- screens/BookingsScreen.js
+++ screens/BookingsScreen.js
@@ -15,6 +15,7 @@ const BookingsScreen = ({navigation, appDuck}) => {
     const [bookings, setBookings] = useState([]);
     const [loading, setLoading] = useState(null);
     const isFocused = useIsFocused();
+    const [emptyBookings, setEmptyBookings] = useState(null)
 
     useEffect(() => {
         if (isFocused) {
@@ -26,7 +27,7 @@ const BookingsScreen = ({navigation, appDuck}) => {
         const queryString = `?limit=${100}`;
         const response = await getAllBookings(queryString);
         setBookings(response?.data?.items);
-        //console.log(response?.data?.items);
+
     }
 
     const getInvitations = async () => {
@@ -34,7 +35,22 @@ const BookingsScreen = ({navigation, appDuck}) => {
 
         const response = await getAllInvitations(queryString);
         setInvitations(response?.data?.items);
+        let validateDate= response?.data?.items.map((invitation) => {
+            const date = new Date();
+            const dateToday = moment(date, 'YYYY-MM-DD');
+            const dateparsed = moment(invitation.booking?.dueDate, 'YYYY-MM-DD');
+            const diff = dateToday.diff(dateparsed, 'days'); 
+            if(diff <= 0 && invitation?.booking){
+                return true
+            }else{
+                return false
+            }
+              
+        })
+       let filterBooking = validateDate.filter(data => data)
+       filterBooking.length > 0 ? setEmptyBookings(false) : setEmptyBookings(true)
     }
+    
 
     const getData = async () => {
         try {
@@ -68,19 +84,25 @@ const BookingsScreen = ({navigation, appDuck}) => {
 
                     <Text textAlign={'center'} mt={10} mb={5} color={Colors.green} fontFamily={'titleComfortaaBold'} fontSize={'2xl'}>MIS RESERVACIONES</Text>
 
-                    {
+                    { !emptyBookings &&
                         invitations.map((invitation, index) => {
                             const date = new Date();
                             const dateToday = moment(date, 'YYYY-MM-DD');
                             const dateparsed = moment(invitation.booking?.dueDate, 'YYYY-MM-DD');
-                            const diff = dateToday.diff(dateparsed, 'days'); 
+                            const diff = dateToday.diff(dateparsed, 'days');                             
                             return (
-                                diff <= 0 &&
+                                diff <= 0 && invitation?.booking &&
                                 <TouchableOpacity key={index} onPress={() => navigation.navigate('BookingsDetailScreen', {invitation_id: invitation.id})}>
                                     <BookinsItem mb={4} dataInvitation={invitation} dataBooking={bookings.find((booking) => booking.id == invitation?.booking?.id)} data={{state: invitation.status}}/>
                                 </TouchableOpacity>
                             );
+                          
                         })
+                    }
+
+                    {
+                       emptyBookings && 
+                       <Text textAlign={'center'} mt={5} mb={5} color={Colors.green} fontFamily={'titleComfortaaBold'} fontSize={'md'}>Sin Reservaciones</Text>
                     }
                 </ScrollView>
             </View>
