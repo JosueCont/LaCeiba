067c2a09797ef917475a16f8976c259a8d622a7c
Isaac Euan <isaac.euan@hiumanlab.com>
2023-03-31 10:26:31 -0600

    se cambio el diseño de la tienda de productos y el detalle del producto con referencia a la maqueta final

--- screens/StoreItem.js
+++ screens/StoreItem.js
@@ -3,12 +3,14 @@ import { Button, Image, Text, View } from "native-base";
 import { Colors } from "../Colors";
 import { MaterialIcons } from "@expo/vector-icons";
 import coins from '../assets/coins.png'
+import shirt from '../assets/shirtPolo.png'
+import bolls from '../assets/bollsGolf.png'
 import { TouchableOpacity } from "react-native";
 
-const StoreItem = ({ navigation, mb = 2 }) => {
+const StoreItem = ({ navigation, mb = 2, image='', title='', price='$ 1,500.00' }) => {
 
     return (
-        <View position={'relative'} flexDirection={'row'} height={150} justifyContent={'center'} alignItems={'flex-start'} bgColor={'#768491'} borderRadius={10} mb={mb}
+        <View position={'relative'} flexDirection={'row'} height={176} justifyContent={'center'} alignItems={'flex-start'} borderRadius={20} mb={mb}
             style={{
                 shadowColor: '#000',
                 shadowOffset: { width: 0, height: 3 },
@@ -16,14 +18,12 @@ const StoreItem = ({ navigation, mb = 2 }) => {
                 shadowRadius: 2,
                 elevation: 3
             }}>
-            <View mr={4} flex={.7} justifyContent={'center'} alignItems={'center'} pt={2}>
-                <Image source={coins} style={{ width: 80, height: 80 }}></Image>
-            </View>
-            <View flex={1} flexDirection={'column'} pr={1} pt={6}>
-                <Text numberOfLines={2} color={'#fff'} fontSize={'18'} justifyItems={'center'} fontFamily={'titleComfortaaBold'}>Puntos para invitados</Text>
-            </View>
-            <View flexDirection={'row'} paddingX={4} alignItems={'center'} justifyContent={'space-between'} width={'100%'} position={'absolute'} height={12} top={'102px'} bgColor={Colors.green} borderRadius={10}>
-                <Text numberOfLines={2} color={Colors.yellow} fontSize={'16'} justifyItems={'center'} fontFamily={'titleComfortaaBold'}>$1,500.00 M.N.</Text>
+                <Image borderRadius={20} source={image=== 'coin' ? coins : image==='shirt' ? shirt : image==='boll' ? bolls : coins } style={{ width: '100%', height: '100%' }}></Image>
+            <View flexDirection={'row'} paddingX={4} alignItems={'center'} justifyContent={'space-between'} width={'100%'} position={'absolute'} height={'60px'} top={'116px'} bgColor={Colors.green} borderBottomRadius={20}>
+                <View flexDirection={'column'}>
+                <Text color={Colors.yellow} fontSize={'14px'} justifyItems={'center'} fontFamily={'titleConfortaaRegular'}>{title}</Text>
+                <Text color={'white'} fontSize={'14px'} justifyItems={'center'} fontFamily={'titleConfortaaRegular'}>{price}</Text>
+                </View>
                 <View>
                     <Button onPress={() => navigation.navigate('StoreItemDetail')} borderRadius={10} width={100} p={1} bg={'#fff'} _pressed={{ backgroundColor: '#d1d1d1' }} _text={{ color: Colors.green, fontFamily: 'titleComfortaaBold' }}>Comprar</Button>
                 </View>
