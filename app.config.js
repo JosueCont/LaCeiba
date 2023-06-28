43595d199fa25d583d5cb22a50b8cb271a903780
Elvira Cardós <elvira.cardos@hiumanlab.com>
2023-06-28 14:31:14 -0600

    features/ajustes IOS

--- app.config.js
+++ app.config.js
@@ -44,13 +44,13 @@ const commonConfig = {
 };
 
 const ios = {
-    buildNumber: "47",
+    buildNumber: "48",
     supportsTablet: false,
     bundleIdentifier: "com.hiumanlab.clublahacienda"
 };
 
 const android = {
-    versionCode: 47,
+    versionCode: 48,
     adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundImage: "./assets/adaptive-icon-bg.png"
